fd58f2a6e298ec23b314476e802dc653dd957bc0
MarwaKotb <66384068+MarwaKotb@users.noreply.github.com>
2020-06-19 15:03:37 +0300

    Update app.js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -39,26 +39,7 @@ const sectionsID = [];
 */
 //create elements and assign some elements with attributes
 
-function createSection() {
-    const mainS = document.querySelector("main");
 
-    const sectionS = document.createElement('section', {"id":"section4", "data-nav":"section 4" })
-   
-    const divS = document.createElement("div");
-    divS.className = "landing__container";
-
-    const hS = document.createElement("h2");
-    hS.textContent = "Section4";
-    
-    const pS = document.createElement("p"); 
-    pS.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.";
-
-    divS.appendChild(hS);
-    divS.appendChild(pS);
-    sectionS.appendChild(divS);
-    mainS.appendChild(sectionS);
-
-  }
 
 /**
  * End Helper Functions
@@ -70,7 +51,7 @@ function createSection() {
 function createNavMenu() {
    
     for(let i = 1; i <= sections.length; i++){
-        let selector = `#section${i}`
+        let selector = `#section${i}`;
         sectionsID.push(selector);
         
     }
@@ -114,7 +95,7 @@ function createNavMenu() {
 
 // Build New Section
 
-createSection();
+
 createNavMenu();
 
 
